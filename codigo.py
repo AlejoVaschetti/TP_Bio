4c90d4d169d69fb45ab1e16c0423afa95d704e9e
AlejoVaschetti <alejovaschetti@hotmail.com>
2025-04-16 12:02:50 -0300

    Version 1.0 (Espero no hacer mas cambios jajajaja)

--- codigo.py
+++ codigo.py
@@ -249,17 +249,31 @@ g2 = [not i for i in consent_ok]
 subj_ok = []
 subj_ok2 = []
 subj_ok3 = []
+subj_scr = []
+subj_usscr = []
+subj_con = []
+
 isna_subj = [i for i in df.iloc[:,7] == df.iloc[:,7]]
 for i in range(len(df)):
     subj_ok.append(True)
     subj_ok2.append(True)
     subj_ok3.append(True)
+    subj_scr.append(True)
+    subj_usscr.append(True)
+    subj_con.append(True)
     if len(str(df.iloc[i,7])) == 9 and str(df.iloc[i,7]).isnumeric():
         subj_ok3[i] = False 
     if df.iloc[i,4] == 2 and df.iloc[i,5] == 2 and df.iloc[i,6] == 2 and df.iloc[i,7] != df.iloc[i,7]:
        subj_ok[i] = False
     elif (df.iloc[i,4] == 1 or df.iloc[i,5] == 1 or df.iloc[i,6] == 1) and df.iloc[i,7] == df.iloc[i,7]:
         subj_ok2[i] = False
+        if df.iloc[i,4] == 1:
+            subj_scr[i] = False
+        if df.iloc[i,5] == 1:
+            subj_usscr[i] = False
+        if df.iloc[i,6] == 1:
+            subj_con[i] = False
+            
 
 
 
@@ -291,26 +305,32 @@ matriz_valores = [a1, a2, b1, b2, b3, c1,c2,c3,bc1,d1,d2,e1,e2,f1,f2,g1,g2,h1,h2
 paciente_limpio = [all(valores) for valores in zip(*matriz_valores)]
 print(sum(paciente_limpio))
 
-tabla = pd.DataFrame(data=[[len(a1)-sum(a1), len(a2)-sum(a2), "-"],
+print(len(set(df.iloc[:,7])) - sum(isna_subj) - 1)
+
+tabla = pd.DataFrame(data=[[len(a1)-sum(a1), len(a2)-sum(a2), sum(paisbien)],
                          [len(b1)-sum(b1), len(b2)-sum(b2) + len(df) - sum(fecha_ok), len(ok_años_dif) - sum(ok_años_dif)],
                          [len(c1)-sum(c1), len(c2)-sum(c2) + len(df) - sum(fecha_ok2), len(ok_años_dif) - sum(ok_años_dif)],
                          [len(d1)-sum(d1), len(d2)-sum(d2), "-"],
-                         [len(e1)-sum(e1), sum(scr_ok) - len(df) + sum(isna_scr), "-"],
-                         [len(f1)-sum(f1), sum(usscr_ok) - len(df) + sum(isna_usscr), "-"],
-                         [len(g1)-sum(g1), sum(consent_ok) - len(df) + sum(isna_consent), "-"],
-                         [len(h1)-sum(h1), len(df) - sum(isna_subj) - sum(subj_ok3) ,len(h2)-sum(h2) + sum(paisbien)]])
+                         [len(e1)-sum(e1), sum(scr_ok) - len(df) + sum(isna_scr), len(h1)-sum(h1) + len(subj_scr) - sum(subj_scr)],
+                         [len(f1)-sum(f1), sum(usscr_ok) - len(df) + sum(isna_usscr), len(h1)-sum(h1) + len(subj_usscr) - sum(subj_usscr)],
+                         [len(g1)-sum(g1), sum(consent_ok) - len(df) + sum(isna_consent), len(h1)-sum(h1) + len(subj_con) - sum(subj_con)],
+                         [len(h1)-sum(h1) + len(h2)-sum(h2), len(df) - sum(isna_subj) - sum(subj_ok3) , len(h1)-sum(h1) + len(h2)-sum(h2) + sum(paisbien)]])
 
 print(tabla)
 
+reglas = pd.DataFrame(data=[['A1','A2','B1','B2','B3','C1','C2','C3','BC1','D1','D2','E1','E2','F1','F2','G1','G2','H1','H2','H3','H4','H5'],
+                         [len(a1)-sum(a1), len(a2)-sum(a2), len(b1)-sum(b1), len(b2)-sum(b2), len(df) - sum(fecha_ok),
+                          len(c1)-sum(c1), len(c2)-sum(c2), len(df) - sum(fecha_ok2), len(ok_años_dif) - sum(ok_años_dif),
+                          len(d1)-sum(d1), len(d2)-sum(d2), len(e1)-sum(e1), sum(scr_ok) - len(df) + sum(isna_scr),
+                          len(f1)-sum(f1), sum(usscr_ok) - len(df) + sum(isna_usscr), len(g1)-sum(g1), sum(consent_ok) - len(df) + sum(isna_consent),
+                          len(h1)-sum(h1), len(h2)-sum(h2), len(df) - sum(isna_subj) - sum(subj_ok3), len(set(df.iloc[:,7])) - sum(isna_subj) - 1, sum(paisbien)]])
 
+print(reglas)
 
 print(h1.index(False, 16))
 print(h2.index(False))
 print(h3.index(False, 16))
 print(h4.index(False))
 
-print(len(set(df.iloc[:,7])))
-
-print("No hay distintos")
-
 tabla.to_csv('tabla.csv', index=False)
+reglas.to_csv('reglas.csv', index=False)
